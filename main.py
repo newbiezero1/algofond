615a99eb913cc36a56bd6db0584a4e30a32db66e
andsnd <kentozavr01@gmail.com>
2024-11-21 15:08:38 +0200

    fix rsi candle

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ for conf in configs:
     utils.log('EMA slow: ' + str(round(v_slowEMA[-1],2)))
     utils.log('EMA filter: ' + str(round(v_filterEMA,2)))
 
-    rsi = utils.calculate_rsi(ohlc, 14)[-2]
+    rsi = utils.calculate_rsi(ohlc, 14)[-1]
     utils.log('RSI: ' + str(round(rsi,2)))
 
     bullSignal = utils.calculate_bullSignal(v_fastEMA, v_slowEMA)[-2]
